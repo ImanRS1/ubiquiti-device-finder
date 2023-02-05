0f9c1b7743cf2488cc48d468c6e8b17086334c4e
Iman Reza Soltani <iman.soltani@bonniernews.se>
2023-02-05 19:08:45 +0100

    temp eslint ignore on build to test vercel problem

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true
+  },
   reactStrictMode: true,
   compiler: {
     styledComponents: true
   }
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
