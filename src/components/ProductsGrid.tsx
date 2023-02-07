c10d4d432e7620543ceac9332d225cef48e947c8
Iman Reza Soltani <iman.soltani@bonniernews.se>
2023-02-07 16:29:15 +0100

    refactor

--- src/components/ProductsGrid.tsx
+++ src/components/ProductsGrid.tsx
@@ -93,7 +93,7 @@ const ProductsWrapper = styled.div`
   display: flex;
   flex-wrap: wrap;
   margin: 0 -12px 0 -12px;
-  min-height: 300px;
+  min-height: 600px;
 
   ${theme.breakpoints.productsViewBreak} {
     justify-content: center;
